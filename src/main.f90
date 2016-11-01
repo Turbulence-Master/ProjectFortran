9602f6c87dd623a3d5e7e30db57f205622785b6c
Guilherme Lindner <lindner.guilherme@gmail.com>
2016-11-01 23:46:33 +0100

    start of midpoint

--- src/main.f90
+++ src/main.f90
@@ -7,9 +7,11 @@ real :: a,b
 
 a = 0
 b = 1
-n = 200
+n = 10
 
 call rectangle(a,b,n)
+
+call midpoint(a,b,n)
 
 call trapezoid(a,b,n)
 
--- src/tools.f90
+++ src/tools.f90
@@ -5,7 +5,10 @@ private
 
 public rectangle
 public trapezoid
+public midpoint
+
 contains
+
     !> Subroutine for using the rectangle method
     !! method
     !! @param a, b Lower and Upper range
@@ -18,14 +21,40 @@ contains
     temp = 0
     h = (b - a) / n
     do i = 0,n-1
-        xi = a + h*i
+        xi = (a + h*i)
         temp = temp + xi**4 -xi**3
         !temp = temp + (3 + 2*xi + 5*xi**2)
+        print*, xi, h
     end do
     res = temp * h
     error = (((b - a)**3)/(24*(n**2)))*10
     print*, 'Rectangle =', res, 'error =', error, 'total =', res + error
     end subroutine
+
+
+    !> Subroutine for using the midpoint method
+    !! method
+    !! @param a, b Lower and Upper range
+    !! @param n Number of domain division
+    !! @todo more test cases
+    subroutine midpoint(a, b, n)
+    implicit none
+    integer :: i,j,n
+    real :: a,b,h,temp,xi,res,error
+    h = (b - a) / n
+    temp = 0
+    do i = 0,n-1
+        xi = (a + h)/2 + h*i
+        temp = temp + h*(xi**4 - xi**3)
+        print*, xi, h
+        !temp = temp + (3 + 2*xi + 5*xi**2)
+    end do
+    res = temp * h
+    error = -((b - a)**3/(12*n**2))*10
+    print*, 'Midpoint =', res, 'error =', error, 'total =', res + error
+   
+    end subroutine
+
 
     !> Subroutine for using the trapezoid method
     !! method
@@ -47,7 +76,6 @@ contains
     res = temp * h/2
     error = -((b - a)**3/(12*n**2))*10
     print*, 'Trapezoid =', res, 'error =', error, 'total =', res + error
-   
 
     end subroutine
 
