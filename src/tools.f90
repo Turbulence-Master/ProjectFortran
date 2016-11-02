08b6bef76a24941737b66d77871c1a08b1b5719f
Guilherme Lindner <lindner.guilherme@gmail.com>
2016-11-02 19:27:31 +0100

    initial trapezoid commit

--- src/tools.f90
+++ src/tools.f90
@@ -20,11 +20,10 @@ contains
     real :: a,b,h,temp,xi,res,error
     temp = 0
     h = (b - a) / n
-    do i = 0,n-1
+    do i = 1,n
         xi = (a + h*i)
-        temp = temp + xi**4 -xi**3
-        !temp = temp + (3 + 2*xi + 5*xi**2)
-        print*, xi, h
+        !temp = temp + xi**4 -xi**3
+        temp = temp + (3 + 2*xi + 5*xi**2)
     end do
     res = temp * h
     error = (((b - a)**3)/(24*(n**2)))*10
@@ -45,12 +44,11 @@ contains
     temp = 0
     do i = 0,n-1
         xi = (a + h)/2 + h*i
-        temp = temp + h*(xi**4 - xi**3)
-        print*, xi, h
-        !temp = temp + (3 + 2*xi + 5*xi**2)
+        !temp = temp + h*(xi**4 - xi**3)
+        temp = temp + (3 + 2*xi + 5*xi**2)
     end do
     res = temp * h
-    error = -((b - a)**3/(12*n**2))*10
+    error = ((b - a)**3/(24*n**2))*10
     print*, 'Midpoint =', res, 'error =', error, 'total =', res + error
    
     end subroutine
@@ -67,11 +65,11 @@ contains
     real :: a,b,h,temp,xi,xi1,res,error
     h = (b - a) / n
     temp = 0
-    do i = 1,n
+    do i = 0,n-1
         xi1 = a + h*(i+1)
         xi = a + h*i
-        temp = temp + (xi1**4 - xi1**3) + (xi**4 - xi**3)
-        !temp = temp + (3 + 2*xi1 + 5*xi1**2) + (3 + 2*xi + 5*xi**2)
+        !temp = temp + (xi1**4 - xi1**3) + (xi**4 - xi**3)
+        temp = temp + (3 + 2*xi1 + 5*xi1**2) + (3 + 2*xi + 5*xi**2)
     end do
     res = temp * h/2
     error = -((b - a)**3/(12*n**2))*10
