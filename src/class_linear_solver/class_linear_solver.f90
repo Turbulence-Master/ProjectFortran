27c82892791cfd05262331b0cfa9240f08c49631
Fannie <fannie.gerosa@yahoo.it>
2016-11-18 10:27:44 +0100

    other change

--- src/class_linear_solver/class_linear_solver.f90
+++ src/class_linear_solver/class_linear_solver.f90
@@ -9,9 +9,8 @@
 !------------------------------------------------------------------------------
 
 module linear_solver
-  use netcdf
   use precision
-! use module from lapack (still work in progress)
+	!use class_field 
 
   implicit none
 
@@ -21,11 +20,6 @@ module linear_solver
   !-> Declare exported procedure
   public :: lu_solver
   public :: thomas_solver
-
-
-	interface 
-!! interface for the subroutine for the LU decomposition from Lapack
-	end interface
 
 
 contains
@@ -41,25 +35,28 @@ contains
   ! REVISION HISTORY:
   ! TODO_dd_mmm_yyyy - TODO_describe_appropriate_changes - TODO_name
   !
-  !> @param[in] inParam: A upper triangular matrix, b right side array (known term)   
+  !> @param[in] inParam: A upper triangular matrix, b right side array (known term), n size of A   
   !> @param[out] outParam: x solution of the system      
   !--------------------------------------------------------------------------- 
 
 	subroutine bksub(n,A,b,x)
 		implicit none 
-		!! define A,b as intent in allocatable, n is the size of the matix
-    !! define x as output same
-		! define i,j
 
-    !catch the error in case of non square matrix
+		integer(ik),intent(in)::n
+		real(rk), intent(in) :: A(n,n), b(n)
+		real(rk), intent(out) :: x(n)
+		
+		real(rk)::i,j		
+		
+		if (A(n,n)==0) stop "null element on the diagonal"
 		x(n)=b(n)/A(n,n)		
 		do i=n-1,1,-1
-		!if element on the diagonal is 0 -> error
+			if (A(i,i)==0) stop "null element on the diagonal" !if element on the diagonal is 0 -> error
 			do j=i+1,n,1
 				x(i)=(b(i)-(A(i,j)*b(j)))/A(i,i)			
 			end do		
 		end do
-
+		
 	end subroutine bksub
 
 	!---------------------------------------------------------------------------  
@@ -72,20 +69,21 @@ contains
   ! REVISION HISTORY:
   ! TODO_dd_mmm_yyyy - TODO_describe_appropriate_changes - TODO_name
   !
-  !> @param[in] inParam: A lower triangular matrix, b right side array (known term)   
+  !> @param[in] inParam: A lower triangular matrix, b right side array (known term), n size of the matrix   
   !> @param[out] outParam: x solution of the system    
   !--------------------------------------------------------------------------- 
 
-	subroutine fwsub(A,b,x)
+	subroutine fwsub(n,A,b,x)
 		implicit none 
-		!! define A,b as intent in allocatable, n is the size of the matix
-    !! define x as output same
-		!! define i,j
-
-    !catch the error in case of non square matrix
+		integer(ik),intent(in)::n
+		real(rk), intent(in) :: A(n,n), b(n)
+		real(rk), intent(out) :: x(n)
+		
+		real(rk)::i,j		
+    if (A(1,1)==0) stop "null element on the diagonal"
 		x(1)=b(1)/A(1,1)		
 		do i=2,n,1
-		!if element on the diagonal is 0 -> error
+			if (A(i,i)==0) stop "null element on the diagonal" !if element on the diagonal is 0 -> error
 			do j=1,i-1,1
 				x(i)=(b(i)-(A(i,j)*b(j)))/A(i,i)			
 			end do		
@@ -95,38 +93,45 @@ contains
 
   !---------------------------------------------------------------------------  
   !> @author 
-  !> Routine Author Name and Affiliation.
+  !> lu_solver
   !
   ! DESCRIPTION: 
-  !> Brief description of routine. 
-  !> @brief
-  !> Flow method (rate of change of position) used by integrator.
-  !> Compute \f$ \frac{d\lambda}{dt} , \frac{d\phi}{dt},  \frac{dz}{dt} \f$
+  !> Solve a linear system with a generic matrix A (square) using the LU 
+  !> decomposition from lapack
+  !> 
+  !> 
   !
   ! REVISION HISTORY:
   ! TODO_dd_mmm_yyyy - TODO_describe_appropriate_changes - TODO_name
   !
-  !> @param[in] inParam      
-  !> @param[out] outParam      
-  !> @return returnValue
+  !> @param[in] n size of A b x, b right side array    
+  !> @param[out] x solution array
+  !> @param[inout] A input as the matrix of coeff but output as combination of LU 
   !---------------------------------------------------------------------------  
   
-	subroutine lu_decomp(n,A,L,U)
-		implicit none
-		!declaration of all the variable with intent, and precision from our module
-		!! solving the linear sistem using decomposition LU from Lapack
-	end subroutine lu_decomp
 	
 	subroutine lu_solver(n,A,b,x)
 		implicit none
-		!declaration of all the variable with intent, and precision from our module
-		!! solving the linear sistem using decomposition LU from Lapack
 
-   !sfrutta lu_decomp per ricavare LU e bksub + fwsub to solve the two system
-		call lu_decomp(n,A,L,U)
+		integer(ik),intent(in)::n
+		real(rk),intent(in)::b(n)
+		real(rk),intent(inout) :: A(n,n)
+		real(rk),intent(out) :: x(n)
+		real(rk)::L(n)
+		real(rk)::i,j	
+
+		!using the subroutine from lapack for the LU decomposition of the matrix A
+		! --> info of the function to add 
+		call dgetrf(n,n,A,n,pivots_array,info)
+		L=A
+		do i=1,n
+			if A(i,i)/=0
+				L(i,i)=1
+			end if
+		end do
 		
 		call fwsub(L,b,y)
-		call bksub(U,y,x)
+		call bksub(A,y,x) ! we use A instead to create a new matrix U
 
 	end subroutine lu_solver
 
@@ -134,28 +139,30 @@ contains
 
   !---------------------------------------------------------------------------  
   !> @author 
-  !> Routine Author Name and Affiliation.
+  !> thomas_algorithm
   !
   ! DESCRIPTION: 
-  !> Brief description of routine. 
-  !> @brief
-  !> Flow method (rate of change of position) used by integrator.
-  !> Compute \f$ \frac{d\lambda}{dt} , \frac{d\phi}{dt},  \frac{dz}{dt} \f$
-  !
+  !> select from A the vector alpha,betha e theta. 
+	!>It works for tridiagonal matrix
+	!
   ! REVISION HISTORY:
   ! TODO_dd_mmm_yyyy - TODO_describe_appropriate_changes - TODO_name
   !
-  !> @param[in] inParam      
-  !> @param[out] outParam      
-  !> @return returnValue
+  !> @param[in] n size of A, A tridiagonal coefficient matrix      
+  !> @param[out] alpha array containing the principal diagola of A,
+	!>             betha lower diagonal elements, 
+	!>             theta upper diagonal elements
   !---------------------------------------------------------------------------  
-!! thomas algorithm create the 3 vector and than we can compute the 
+ 
 	subroutine thomas_algorithm(n,A,alpha,betha,theta)
 		implicit none 
-		!!declaration of all the variable with intent, and precision from our module
-		!integer(ik), intent(in)::n
-		!real(rk),intent(in)::A(:,:) ...
+		
+		integer(ik),intent(in)::n
+		real(rk), intent(in) :: A(n,n)
+		real(rk), intent(out) :: alpha(n),betha(n-1),theta(n-1)
+		real(rk)::i	
 		!! alpha array of n elements, betha n-1 elements, theta n-1 elements
+		
 		alpha(1)=A(1,1)
 		do i=2,n
     	betha(i-1)=A(i,i-1)/alpha(i-1)
@@ -165,12 +172,31 @@ contains
 
 	end subroutine thomas_algorithm
 
+
+  !---------------------------------------------------------------------------  
+  !> @author 
+  !> thomas_solver
+  !
+  ! DESCRIPTION: 
+  !> Using the vector alpha,betha,theta from thomas_algorithm solve the 
+	!> tridiagonal system
+	!
+  ! REVISION HISTORY:
+  ! TODO_dd_mmm_yyyy - TODO_describe_appropriate_changes - TODO_name
+  !
+  !> @param[in] inParam: A tridiagonal matrix, b right side array (known term), n size of A   
+  !> @param[out] outParam: x solution of the system  
+  !---------------------------------------------------------------------------  
+ 
 	subroutine thomas_solver(n,A,b,x)
 		implicit none
-		!declaration of all the variable with intent, and precision from our module
-!
-!  all declaration of variables
-!
+		integer(ik),intent(in)::n
+		real(rk), intent(in) :: A(n,n),b(n)
+		real(rk), intent(out) :: x(n)
+		real(rk) :: alpha(n),betha(n-1),theta(n-1)
+		real(rk)::i
+		!! alpha array of n elements, betha n-1 elements, theta n-1 elements
+		
 		call thomas_algorithm(n,A,alpha,betha,theta)
 		
 		y(1)=b(1)		
@@ -179,7 +205,7 @@ contains
 		end do
 
 		x(n)=y(n)/alpha(n)		
-		do i=n-1,1
+		do i=n-1,1,-1
 			x(i)=(y(i)-theta(i)*x(i+1))/alpha(i)		
 		end do
 
@@ -187,9 +213,9 @@ contains
 
 
 
-	subroutine print_sol(x)
-		implicit none
-		!subroutine for print the solution on a file maybe
-	end subroutine print_sol
+!	subroutine print_sol(x)
+!		implicit none
+!	  !subroutine for print the solution on a file maybe
+!	end subroutine print_sol
    
 end module linear_solver
